2293a10f4aa4198a27b14bbe8e5ba71d8b68f633
Erin Shaben <eshaben@icloud.com>
2017-06-25 16:20:47 -0600

    updated! data is rendering on page

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  window.onload = function () {
-    $('#entrance-button').click(function(){
+  window.onload = function() {
+    $('#entrance-button').click(function() {
       $('#modal').hide()
     })
-  document.getElementById('exit-button').onclick = function () {
+    $('#exit-button').click(function() {
       $('.modal-text').text("Sorry, you must be a valid Medical Marijuana patient or at least 21 years old.")
       $('.open-buttons').hide()
-    };
-};
+    })
+  };
 
   $(".button-collapse").sideNav({
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true, // Choose whether you can drag to open on touch screens,
+    closeOnClick: true,
+    draggable: true,
     onOpen: function(el) {
       $('.blur-me').addClass("blurred")
     },
@@ -21,49 +21,50 @@ $(document).ready(function() {
   });
 
   var userInput = ''
-  var baseURL= 'https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/'
+  var baseURL = 'https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/'
 
   $('.submit-button').click(function() {
-      event.preventDefault()
-      var getSearchQuery = $('.search-field')
-      userInput = 'search/' + (getSearchQuery.val()).replace(/ /g, "_")
-      console.log(userInput);
+    event.preventDefault()
+    var getSearchQuery = $('.search-field')
+    userInput = 'search/' + (getSearchQuery.val()).replace(/ /g, "_")
+    console.log(userInput);
 
-      $.ajax({
-          url: baseURL + userInput,
+    $.ajax({
+      url: baseURL + userInput,
+      type: "GET",
+      dataType: "json",
+      headers: {
+        "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
+      },
+      success: function(data) {
+        $('.results').prepend('<img class="leaf-border scroll-here" src="PotLeafBorder.png">')
+        $('.main-results-title').text('Results For ' + data.data[0].name)
+
+
+        var ucpc = data.data[0].ucpc
+        console.log(data.data[0]);
+        var ucpcURL = ucpc + "/effectsFlavors"
+
+        $.ajax({
+          url: baseURL + ucpcURL,
           type: "GET",
           dataType: "json",
           headers: {
             "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
           },
           success: function(data) {
-
-            var ucpc = data.data[0].ucpc
-            console.log(data.data[0]);
-            var ucpcURL = ucpc + "/effectsFlavors"
-
-            $.ajax({
-              url: baseURL + ucpcURL,
-              type: "GET",
-              dataType: "json",
-              headers: {
-                "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
-              },
-              success: function(data) {
-                $.each(data.data, function(key,value){
-                  console.log(key + ":" + value)
-                })
-                console.log(data.data);
-              }
-            });
-
+            $.each(data.data, function(key, value) {
+              $('.results').append('<h5>'+key+'</h5>').addClass('results-title')
+              $('.results').append('<p>'+value+'</p>').addClass('results-text')
+              console.log(key + ":" + value)
+            })
+            console.log(data.data);
           }
-      });
+        });
+      }
+    });
+    $('html, body').animate({
+      scrollTop: $('.results').offset().top
+    }, 2000)
   })
 })
-
-
-// $.get(url)
-// .then(function(data){
-//   console.log(data);
-// })
