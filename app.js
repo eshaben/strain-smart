2c2b4252a3a06077dea9fed2e7f91a4dafc85b4b
Erin Shaben <eshaben@icloud.com>
2017-06-28 16:14:30 -0600

    updated

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   window.onload = function() {
     $('#entrance-button').click(function() {
@@ -12,6 +14,19 @@ $(document).ready(function() {
   var userInput = ''
   var baseURL = 'https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/'
 
+  // $(function autoComplete(){
+  //   var strains = ['Good Medicine', 'Agent Orange', 'Critical Mass', 'Critical Kush',
+  //     'LSD', 'Durango OG', 'Blue Dream', 'Girl Scout Cookies', 'Jack Herer',
+  //     'Lemon Skunk', 'Harlequin', 'OG Kush', 'Gorilla Glue #4', 'XJ-13', 'Amnesia Haze',
+  //     'Durban Poison', 'Bruce Banner', 'Sour Amnesia'
+  //   ]
+  //
+  //   $('#search').autocomplete({
+  //     source: strains
+  //   })
+  // })
+
+
   $('.submit-button').click(function() {
     event.preventDefault()
     var getSearchQuery = $('.search-field')
@@ -19,6 +34,7 @@ $(document).ready(function() {
     console.log(userInput);
     $('#test4').empty()
     $('#test5').empty()
+    $('.error-message').empty()
 
     $.ajax({
       url: baseURL + userInput,
@@ -28,61 +44,80 @@ $(document).ready(function() {
         "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
       },
       success: function(data) {
-        $('.main-results-title').text(data.data[0].name)
-        $('.strain-image').attr('src', data.data[0].image)
-        $('.card-text').text(data.data[0].genetics.names)
+        console.log(data.data);
+        if (data.data.length === 0) {
+          $('.error-message').text("Sorry, we currently do not have information available for this strain")
+        } else {
+          $('.main-results-title').text(data.data[0].name)
+          $('.strain-image').attr('src', data.data[0].image)
 
-        var ucpc = data.data[0].ucpc
-        console.log(data.data[0]);
-        var ucpcURL = ucpc + "/effectsFlavors"
-
-        $.ajax({
-          url: baseURL + ucpcURL,
-          type: "GET",
-          dataType: "json",
-          headers: {
-            "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
-          },
-          success: function(data) {
-            var effectsFlavors = []
-            var effects = []
-            var flavors = []
-
-            $.each(data.data, function(key, value){
-              key = (key[0].toUpperCase() + key.slice(1)).replace(/_/g, " ")
-              value = ((value/10)*100).toFixed(1) + '%'
-              effectsFlavors.push(key = {key, value})
-            })
-            for (var i =0; i<= 5; i++){
-              effects.push(effectsFlavors[i])
-            }
-            effects.push(effectsFlavors[12])
-            for (var i=6; i <= 11; i++){
-              flavors.push(effectsFlavors[i])
-            }
-
-          $.each(effects, function(key, value){
-            $('#test4').append('<h5 class="results-para">'+this.key+': '+this.value+'</h5>')
-            $('#test4').append('<div class="progress result-data"><div class="determinate" style="width:'+this.value+'"></div></div>')
-          })
-          $.each(flavors, function(key, value){
-            $('#test5').append('<h5 class="results-para">'+this.key+': '+this.value+'</h5>')
-            $('#test5').append('<div class="progress result-data"><div class="determinate" style="width:'+this.value+'"></div></div>')
-          })
-
-          $('.search-again').click(function(){
-            $(window).scrollTop(0)
-          })
-          //for search bar on bottom of page
-          //  $('.results').append('<form class="search-form"><div class="input-field"><input id="search" class="search-field" type="search" value="" placeholder="e.g. Jack Herer" required><label class="label-icon" for="search"><i class="material-icons small">search</i></label><i class="material-icons close-icon inline">close</i><button class="submit-button" type="submit" name="button">Go!</button></div></form>')
-
-           $('.hide').removeClass()
+          if (data.data[0].genetics.names === false) {
+            $('.card-text').text("Not Currently Available")
+          } else {
+            $('.card-text').text(data.data[0].genetics.names)
           }
-        });
+
+          var ucpc = data.data[0].ucpc
+          console.log(data.data[0]);
+          var ucpcURL = ucpc + "/effectsFlavors"
+
+          $.ajax({
+              url: baseURL + ucpcURL,
+              type: "GET",
+              dataType: "json",
+              headers: {
+                "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
+              },
+              success: function(data) {
+
+                if (data.data === false) {
+                  $('.error-message').text("Currently No Information Available")
+                } else {
+                  var effectsFlavors = []
+                  var effects = []
+                  var flavors = []
+
+                  $.each(data.data, function(key, value) {
+                    key = (key[0].toUpperCase() + key.slice(1)).replace(/_/g, " ")
+                    value = ((value / 10) * 100).toFixed(1) + '%'
+                    effectsFlavors.push(key = {
+                      key,
+                      value
+                    })
+                  })
+                  for (var i = 0; i <= 5; i++) {
+                    effects.push(effectsFlavors[i])
+                  }
+                  effects.push(effectsFlavors[12])
+                  for (var i = 6; i <= 11; i++) {
+                    flavors.push(effectsFlavors[i])
+                  }
+
+                  $.each(effects, function(key, value) {
+                    $('#test4').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
+                    $('#test4').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
+                  })
+                  $.each(flavors, function(key, value) {
+                    $('#test5').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
+                    $('#test5').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
+                  })
+                  $('.search-again').click(function() {
+                    $(window).scrollTop(0)
+                  })
+                  $('.hide').removeClass()
+                }
+              }
+            })
+            .then(function() {
+              var container = $('body')
+              var scrollTo = $('.leaf-border')
+              $('body').scrollTop(
+                scrollTo.offset().top - container.offset().top + container.scrollTop()
+              )
+            })
+        }
       }
     });
-    $('html, body').animate({
-      scrollTop: $('.results').offset().top
-    }, 2000)
+
   })
 })
---
+++ strainlist.js
@@ -0,0 +1,4 @@
+var strains = ['Good Medicine', 'Agent Orange', 'Critical Mass', 'Critical Kush',
+ 'LSD', 'Durango OG', 'Blue Dream', 'Girl Scout Cookies', 'Jack Herer',
+ 'Lemon Skunk', 'Harlequin', 'OG Kush', 'Gorilla Glue #4', 'XJ-13', 'Amnesia Haze',
+'Durban Poison', 'Bruce Banner', 'Sour Amnesia']
