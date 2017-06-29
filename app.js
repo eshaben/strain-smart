b72851c37dd9370ab43ebb9f72d5ad773968cd67
Erin Shaben <eshaben@icloud.com>
2017-06-29 17:26:29 -0600

    updated 🙌

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
 $(document).ready(function() {
-  window.onload = function() {
-    $('#entrance-button').click(function() {
-      $('#modal').hide()
-    })
-    $('#exit-button').click(function() {
-      $('.modal-text').text("Sorry, you must be a valid Medical Marijuana patient or at least 21 years old.")
-      $('.open-buttons').hide()
-    })
-  };
+  window.onload = loadModal;
 
   var userInput = ''
   var baseURL = 'https://cors-anywhere.herokuapp.com/https://www.cannabisreports.com/api/v1.0/strains/'
 
   $('.submit-button').click(function() {
     event.preventDefault()
-    var getSearchQuery = $('.search-field')
-    userInput = 'search/' + (getSearchQuery.val()).replace(/ /g, "_")
-    $('#test4').empty()
-    $('#test5').empty()
-    $('.error-message').empty()
-
-    $('.loader').removeClass('loader-hide')
+    getInput();
+    emptyResults();
+    showLoader();
 
     $.ajax({
       url: baseURL + userInput,
@@ -31,16 +19,11 @@ $(document).ready(function() {
       },
       success: function(data) {
         if (data.data.length === 0) {
-          $('.error-message').text("Sorry, we currently do not have information available for this strain")
+          noStrainError(data)
         } else {
-          $('.main-results-title').text(data.data[0].name)
-          $('.strain-image').attr('src', data.data[0].image)
-
-          if (data.data[0].genetics.names === false) {
-            $('.card-text').text("Not Currently Available")
-          } else {
-            $('.card-text').text(data.data[0].genetics.names)
-          }
+          appendName(data);
+          appendImage(data);
+          checkGenetics(data);
 
           var ucpc = data.data[0].ucpc
           var ucpcURL = ucpc + "/effectsFlavors"
@@ -53,62 +36,129 @@ $(document).ready(function() {
                 "x-api-key": "a1d66090af7e5d15f41a6007df11f4567ee91f3a"
               },
               success: function(data) {
-
                 if (data.data === false) {
-                  $('.loader').addClass('loader-hide')
-                  $('.error-message').text("Currently No Information Available")
+                  hideLoader(data);
+                  noStrainError(data);
                 } else {
-                  var effectsFlavors = []
-                  var effects = []
-                  var flavors = []
-
-                  $.each(data.data, function(key, value) {
-                    key = (key[0].toUpperCase() + key.slice(1)).replace(/_/g, " ")
-                    value = ((value / 10) * 100).toFixed(1) + '%'
-                    effectsFlavors.push(key = {
-                      key,
-                      value
-                    })
-                  })
-                  for (var i = 0; i <= 5; i++) {
-                    effects.push(effectsFlavors[i])
-                  }
-                  effects.push(effectsFlavors[12])
-                  for (var i = 6; i <= 11; i++) {
-                    flavors.push(effectsFlavors[i])
-                  }
-
-                  $.each(effects, function(key, value) {
-                    $('#test4').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
-                    $('#test4').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
-                  })
-                  $.each(flavors, function(key, value) {
-                    $('#test5').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
-                    $('#test5').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
-                  })
-                  $('.search-again').click(function() {
-                    $(window).scrollTop(0)
-                  })
-                  $('.hide').removeClass()
+                  renderData(data)
                 }
               }
             })
             .then(function() {
-              var container = $('body')
-              var scrollTo = $('.leaf-border')
-              $('.loader').addClass('loader-hide')
-              $('body').scrollTop(
-                scrollTo.offset().top - container.offset().top + container.scrollTop()
-              )
+              scrollToResults()
             })
         }
       },
       error: function(error) {
         var err = error.responseJSON.message
-        $('.loader').addClass('loader-hide')
+        hideLoader(data);
         $('.error-message').text(err)
       }
     });
-    $('.search-field').val('');
+    emptySearchField();
   })
+
+
+
+  function hideModal() {
+    $('#modal').hide()
+  }
+
+  function modalNoResponse() {
+    $('.modal-text').text("Sorry, you must be a valid Medical Marijuana patient or at least 21 years old.")
+    $('.open-buttons').hide()
+  }
+
+  function loadModal() {
+    $('#entrance-button').click(hideModal)
+    $('#exit-button').click(modalNoResponse)
+  }
+
+  function getInput() {
+    var getSearchQuery = $('.search-field')
+    userInput = 'search/' + (getSearchQuery.val()).replace(/ /g, "_")
+  }
+
+  function emptyResults() {
+    $('#test4').empty()
+    $('#test5').empty()
+    $('.error-message').empty()
+  }
+
+  function showLoader() {
+    $('.loader').removeClass('loader-hide')
+  }
+
+  function checkGenetics(data) {
+    if (data.data[0].genetics.names === false) {
+      $('.card-text').text("Not Currently Available")
+    } else {
+      $('.card-text').text(data.data[0].genetics.names)
+    }
+  }
+
+  function appendName(data) {
+    $('.main-results-title').text(data.data[0].name)
+  }
+
+  function appendImage(data) {
+    $('.strain-image').attr('src', data.data[0].image)
+  }
+
+  function noStrainError(data) {
+    $('.error-message').text("Sorry, we currently do not have information available for this strain")
+  }
+
+  function hideLoader(data) {
+    $('.loader').addClass('loader-hide')
+  }
+
+  function scrollToResults() {
+    var container = $('body')
+    var scrollTo = $('.leaf-border')
+    $('.loader').addClass('loader-hide')
+    $('body').scrollTop(
+      scrollTo.offset().top - container.offset().top + container.scrollTop()
+    )
+  }
+
+  function emptySearchField() {
+    $('.search-field').val('');
+  }
+
+  function renderData(data) {
+    var effectsFlavors = []
+    var effects = []
+    var flavors = []
+
+    $.each(data.data, function(key, value) {
+      key = (key[0].toUpperCase() + key.slice(1)).replace(/_/g, " ")
+      value = ((value / 10) * 100).toFixed(1) + '%'
+      effectsFlavors.push(key = {
+        key,
+        value
+      })
+    })
+
+    for (var i = 0; i <= 5; i++) {
+      effects.push(effectsFlavors[i])
+    }
+    effects.push(effectsFlavors[12])
+    for (var i = 6; i <= 11; i++) {
+      flavors.push(effectsFlavors[i])
+    }
+
+    $.each(effects, function(key, value) {
+      $('#test4').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
+      $('#test4').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
+    })
+    $.each(flavors, function(key, value) {
+      $('#test5').append('<h5 class="results-para">' + this.key + ': ' + this.value + '</h5>')
+      $('#test5').append('<div class="progress result-data"><div class="determinate" style="width:' + this.value + '"></div></div>')
+    })
+    $('.search-again').click(function() {
+      $(window).scrollTop(0)
+    })
+    $('.hide').removeClass()
+  }
 })
